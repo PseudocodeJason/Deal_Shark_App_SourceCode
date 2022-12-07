288d727e371facd5f31dd9f95d66a82d2a64f488
RandomPropertyPseudoCode <93780112+RandomPropertyPseudoCode@users.noreply.github.com>
2022-12-06 19:01:27 -0500

    API added to homepage

--- App.js
+++ App.js
@@ -2,7 +2,6 @@
 // import store from "./redux/store";
 // import DefaultApp from "./defaultApp";
 // import Home from "./dealsApp"
-// //test comment hello world
 // export default function App() {
 //   return (
 //     <Provider store={store}>
@@ -12,29 +11,58 @@
 // }
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { useEffect, useState } from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 const Stack = createStackNavigator();
+const API = 'https://www.cheapshark.com/api/1.0/deals?'
+
+
 
 
 export default function App() {
+
   return (
     <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={HomeScreen}/>
-            </Stack.Navigator>
-        </NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const HomeScreen = ({navigation}) =>{
-  return(
+const HomeScreen = ({ navigation }) => {
+  const [data, setData] = useState()
+  useEffect(() => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((json) => setData(json))
+
+  }, []);
+
+  const cheapGames = ({ item }) => {
+    return (
       <View>
-          <Text>Test Page</Text>
+        <Text>Name: {item.title}</Text>
+        <Text>Regular Price: $ {item.normalPrice}</Text>
+        <Text>On Sale Price: $ {item.salePrice}</Text>
       </View>
+    )
+  }
+
+  return (
+    <View>
+      <Text>Test Page</Text>
+      <FlatList
+        data={data}
+        renderItem={cheapGames}
+      />
+    </View>
   )
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
