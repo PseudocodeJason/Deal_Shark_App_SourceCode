a38c46a1bf6113f9c740f76c0a2db3437efc2d1a
Jason <jason.tieu@Hotmail.com>
2022-12-07 19:38:11 -0500

    Added Clickable Games Title & Games Detail Screen

--- dealsApp.js
+++ dealsApp.js
@@ -1,8 +1,8 @@
-import { Button, FlatList, StyleSheet, Text, TextInput, View } from "react-native";
+import { Button, FlatList, TouchableHighlight, StyleSheet, Text, TextInput, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { useState, useEffect } from "react";
-import { SearchGame } from "./redux/actions";
+import { SearchGameTitle, SearchGameID } from "./redux/actions";
 import { connect } from "react-redux";
 const Stack = createStackNavigator();
 
@@ -12,22 +12,23 @@ function App() {
             <Stack.Navigator>
                 <Stack.Group>
                     <Stack.Screen name="Home" component={HomeScreen} />
-                    <Stack.Screen name="Search" component={Search}/>
+                    <Stack.Screen name="Search" component={SearchTitle} />
+                    <Stack.Screen name="Game" component={Game} />
                 </Stack.Group>
             </Stack.Navigator>
         </NavigationContainer>
     )
 }
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({ navigation }) => {
     const [search, setSearch] = useState("")
 
     return (
         <View>
-            <Text>Test Page</Text>
             <TextInput
                 onChangeText={setSearch}
                 value={search}
+                style={styles.input}
             />
             <Button title="Search"
                 onPress={() => navigation.navigate('Search', { searchText: search })}
@@ -36,42 +37,101 @@ const HomeScreen = ({navigation}) => {
     )
 }
 
-const SearchScreen = ({navigation, route, SearchGame, apiList}) => {
-    const {searchText} = route.params
+const SearchScreen = ({ navigation, route, SearchGameTitle, apiList }) => {
+    const { searchText } = route.params
     const [search, setSearch] = useState("")
 
     const SearchResults = ({ item }) => {
+
         return (
-            <View>
-                <Text>Title: {item.external}</Text>
-                <Text>Sale Price: {item.cheapest}</Text>
-                <Text>Regular Price: {item.gameID}</Text>
-            </View>
+            <TouchableHighlight onPress={() => navigation.navigate('Game', { gameID: item.gameID })}>
+                <View style={styles.item}>
+                    <Text>Title: {item.external}</Text>
+                    <Text>Sale Price: {item.cheapest}</Text>
+                    <Text>Regular Price: {item.gameID}</Text>
+                </View>
+            </TouchableHighlight>
+
         )
     }
-    
-    useEffect(() =>{
+
+    useEffect(() => {
         fetch("https://www.cheapshark.com/api/1.0/games?title=" + searchText)
-                    .then((res) => res.json())
-                    .then((json) => SearchGame(json))
+            .then((res) => res.json())
+            .then((json) => SearchGameTitle(json))
     }, [])
-    return(
+    return (
         <View>
-             <TextInput
+            <TextInput
                 onChangeText={setSearch}
                 value={search}
+                style={styles.input}
             />
             <Button title="Search"
                 onPress={() => fetch("https://www.cheapshark.com/api/1.0/games?title=" + search)
                     .then((res) => res.json())
-                    .then((json) => SearchGame(json))}
+                    .then((json) => SearchGameTitle(json))}
             />
             <FlatList data={apiList} renderItem={SearchResults} />
         </View>
     )
 }
-const mapDispastchHome = { SearchGame }
-const mapStateHome = (state) => ({ apiList: state.apiSearch.apiList })
-const Search = connect(mapStateHome, mapDispastchHome)(SearchScreen)
+
+
+const GameScreen = ({ navigation, route, game, SearchGameID }) => {
+    const { gameID } = route.params
+    const [search, setSearch] = useState("")
+    const [title, setTitle] = useState("")
+
+    useEffect(() => {
+        fetch("https://www.cheapshark.com/api/1.0/games?id=" + gameID)
+            .then((res) => res.json())
+            .then((json) => SearchGameID(json))
+    }, [])
+
+    const GameResults = ({ item }) => {
+        return (
+            <View style={styles.item}>
+                <Text>Store Id: {item.storeID}</Text>
+                <Text>Price: {item.price}</Text>
+                <Text>Regular Price: {item.retailPrice}</Text>
+            </View>
+        )
+    }
+
+    return (
+        <View>
+            <Text style={{ textAlign: "center" }}>{game.info.title}</Text>
+            <FlatList data={game.deals} renderItem={GameResults} />
+        </View>
+    )
+
+}
+const styles = StyleSheet.create({
+    item: {
+        borderWidth: 1,
+        margin: 5,
+        borderColor: 'black',
+        padding: 10,
+        borderRadius: 4,
+        backgroundColor: 'white'
+    },
+    input: {
+        height: 44,
+        padding: 10,
+        marginTop: 10,
+        marginBottom: 10,
+        backgroundColor: '#e8e8e8',
+    },
+
+});
+
+const mapDispastchTitle = { SearchGameTitle }
+const mapStateTitle = (state) => ({ apiList: state.apiSearch.apiList })
+const SearchTitle = connect(mapStateTitle, mapDispastchTitle)(SearchScreen)
+
+const mapDispastchGame = { SearchGameID }
+const mapStateGame = (state) => ({ game: state.apiFindGame.game })
+const Game = connect(mapStateGame, mapDispastchGame)(GameScreen)
 
 export default (App)
