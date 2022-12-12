55fb32a6dbc7493c29b52bda7de072853c45bec9
Jason <jason.tieu@Hotmail.com>
2022-12-12 18:56:25 -0500

    Added checks inactive stores and the removal it

--- redux/reducers/apiAllStores.js
+++ redux/reducers/apiAllStores.js
@@ -9,9 +9,19 @@ export default (state = initial_state, action)=>{
         console.log(JSON.stringify(action));
         var storesList = action.payload.allStores
         console.log(storesList)
+        //Searches Through The store and looks for activestores and adds them to the new array
+        var activeStores = []
+        storesList.forEach(element => {
+            console.log(element)
+            if (element.isActive == "1"){
+                console.log("match")
+                activeStores = [...activeStores, element]
+            }
+        });
+        console.log(activeStores)
         return{
             ...state,
-            allStores: storesList
+            allStores: activeStores
         }
     }
     return state;
